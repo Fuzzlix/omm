2af6c5103180166148d8d7657bc15091c8d601e9
Ulrich Schmidt <u.sch.zw@gmx.de>
2016-08-16 15:44:33 +0200

    updated iuplua.mk
    Using the new include feature to load luastrip- and bin2c- rules.

--- iuplua.mk
+++ iuplua.mk
@@ -13,36 +13,12 @@ make "iup"
 --
 local LUAVER  = make.Needs"lua".LUAVERSION
 local TEMPDIR = TEMPDIR.."/"..LUAVER
-
-include "luastrip" -- load strip function.
-local function bin2c(par)
-  local function msg(s, ...) return ("lua2c(): "..s):format(...); end;
-  --
-  local numtab={}; 
-  for i = 0, 255 do numtab[string.char(i)] = ("%3d,"):format(i); end;
-  --
-  local ifname = par.source:gsub(".*/", "");
-  local ifile  = io.open(par.source,  "rb");
-  if not ifile then return nil, msg("cant open %s.",par.source); end;
-  local ofile  = io.open(par.outfile, "w+");
-  if not ofile then 
-    ifile:close();
-    return nil, msg("cant open %s.",par.outfile); 
-  end;
-  local res = ofile:write(
-      "\n{\n static const unsigned char B1[]={\n",
-      ifile:read("*a"):gsub(".", numtab):gsub(("."):rep(80), "%0\n"), 
-      string.format("\n};\n\n  iuplua_dobuffer(L,(const char*)B1,sizeof(B1),%q);\n}\n", ifname)
-      );
-  ofile:close();
-  ifile:close();
-  if res then return true; end;
-  return nil, msg("error writing %s.",par.outfile); 
-end;
-
+--
+include "luastrip" -- load luastrip rule.
+include "bin2c"    -- load bin2c rule.
 --
 if not make.Targets("iuplua"..LUAVER) then -- iuplua-3.19.1
-  -- [[
+  --
   local LUA_CDIR = LUA_BIN.."/lib/" .. LUAVER
   local SRCDIR   = IUPROOT .. "/iup/srclua5"
   local INCLUDES = TEMPDIR.." . ../include ../src"
@@ -55,24 +31,23 @@ if not make.Targets("iuplua"..LUAVER) then -- iuplua-3.19.1
                     radio val tabs fontdlg tree progressbar messagedlg progressdlg backgroundbox flatbutton \z
                     animatedlabel calendar datepick param parambox"
 
-  local CTRLC    = addprefix("elem/il_", addsuffix( ".c",   CTRL))
-  local SRCLUA   = addprefix("elem/",    addsuffix( ".lua", CTRL)).. " " .. "iuplua.lua constants.lua iup_config.lua" 
+  local SRCLUA   = addprefix("elem/",    addsuffix( ".lua", CTRL)).." iuplua.lua constants.lua iup_config.lua" 
   
-  local STRPLUA  = rule {base=SRCDIR, odir=TEMPDIR, func=luastrip, src=SRCLUA, 
-                         action = "lstrip $SOURCE $OUTFILE"
-                        }
-  local CTRLH    = rule {base=SRCDIR, odir=TEMPDIR, func=bin2c, outext=".lh", inputs=STRPLUA,
-                         action = "lua2c $SOURCE $OUTFILE" -- dummy commandline for console output.
-                        }
+  local STRPLUA  = luastrip {base=SRCDIR, odir=TEMPDIR, src=SRCLUA} -- luastrip: imported rule
+
+  local CTRLH    = bin2c {inputs=STRPLUA, base=SRCDIR, odir=TEMPDIR, outext=".lh", -- bin2c: imported rule
+                          command="iuplua_dobuffer(L,(const char*)B1,sizeof(B1),%q)"
+                         }
   local SRC      = "iuplua.c iuplua_api.c iuplua_draw.c iuplua_tree_aux.c iuplua_scanf.c \z
-                    iuplua_getparam.c iuplua_getcolor.c iuplua_config.c " .. CTRLC
+                    iuplua_getparam.c iuplua_getcolor.c iuplua_config.c " .. 
+                    addprefix("elem/il_", addsuffix( ".c", CTRL))
   --
-  local DLIBOBJ = c99 {"iuplua"..LUAVER, src=SRC, base=SRCDIR, odir=TEMPDIR, incdir=INCLUDES, 
-                       defines=DEFINES, cflags=CFLAGS, deps=CTRLH, needs=NEEDS
-                      }
-  local DLIB    = c99.shared {"iuplua"..LUAVER, inputs=DLIBOBJ, odir=LUA_CDIR, cflags=CFLAGS, needs=NEEDS}
+  local DLIBOBJ  = c99 {"iuplua"..LUAVER, src=SRC, base=SRCDIR, odir=TEMPDIR, incdir=INCLUDES, 
+                        defines=DEFINES, cflags=CFLAGS, needs=NEEDS, deps=CTRLH -- deps defined here explicit, because automatic ..
+                       }                                                        -- include file checks do not work for generated files (yet).
+  local DLIB     = c99.shared {"iuplua"..LUAVER, inputs=DLIBOBJ, odir=LUA_CDIR, cflags=CFLAGS, needs=NEEDS}
   --
   target("iuplua"..LUAVER, DLIB)
   default{DLIB}
-  --]]
+  --
 end;
